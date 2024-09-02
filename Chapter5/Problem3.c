dbbec628753c6245eea4f2b9b8942ea1533c2511
lt47 <55928190+lt47@users.noreply.github.com>
2024-09-01 19:16:00 -0500

    got a working pointer version of strcat

--- Chapter5/Problem3.c
+++ Chapter5/Problem3.c
@@ -1,44 +1,48 @@
 #include <stdio.h>
 /*strcat: concatenate t to the end of s; s must be big enough*/
-void original_strcat(char s[], char t[]){
+void original_strcat(char s[], char t[])
+{
     int i, j;
 
     i = j = 0;
 
-    //while (s[i] != '\0'){i++;} /* find end of s*/
-    /* So I just discovered that while (s[i] != '\0') is equivalent to while(s[i]). 
-    * while(s[i]) just evaluates until it reaches a null character/endpoint/terminator i.e. '\0' - the ASCII value of zero.
-    */
-    while (s[i]){i++;} /* find end of s*/
+    // while (s[i] != '\0'){i++;} /* find end of s*/
+    /* So I just discovered that while (s[i] != '\0') is equivalent to while(s[i]).
+     * while(s[i]) just evaluates until it reaches a null character/endpoint/terminator i.e. '\0' - the ASCII value of zero.
+     */
+    while (s[i])
+    {
+        i++;
+    } /* find end of s*/
     /* copy t */
     while ((s[i++] = t[j++]) != '\0')
         ;
 
-    printf(s);
+    printf("%s", s);
 }
 
 /* In exercise 3, I am asked to create a pointer version of the above. */
 /*I have learned that C does not have a pass by reference as C++ does.*/
-void new_strcat(char *s[], char *t[]){
-    int i, j;
+void new_strcat(char *s, char *t)
+{
 
-    i = j = 0;
-
-    while (s[i] != '\0'){i++;} /* find end of s*/
+    while (*s)
+    {
+        s++;
+    } /* find end of s*/
 
     /* copy t */
-    while ((s[i++] = t[j++]) != '\0')
+    while ((*s++ = *t++))
         ;
 
-    printf(s);
+    printf("%s", t);
 }
 
-int main(){
+int main()
+{
     char firstStr[] = "test";
     char secondStr[] = "not";
 
-    //new_strcat(&firstStr, &secondStr);
-    original_strcat(firstStr, secondStr);
-
+    new_strcat(firstStr, secondStr);
+    // original_strcat(firstStr, secondStr);
 }
-
