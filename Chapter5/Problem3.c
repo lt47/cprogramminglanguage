5d6e5781132ccf49648a3cae0dbdbc8ac820bc78
lt47 <layetee@gmail.com>
2024-09-02 07:31:39 -0500

    further understanding of 5.3 regarding pointers

--- Chapter5/Problem3.c
+++ Chapter5/Problem3.c
@@ -31,11 +31,12 @@ void new_strcat(char *s, char *t)
         s++;
     } /* find end of s*/
 
-    /* copy t */
+    /* copy t to end of s*/
     while ((*s++ = *t++))
         ;
 
-    printf("%s", t);
+    /*It doesnt make sense to me why t is the pointer that is modified here when all of t is supposed to be added to the end of s. See Line 50 notes.*/
+    printf("%s\n", t);
 }
 
 int main()
@@ -43,6 +44,13 @@ int main()
     char firstStr[] = "test";
     char secondStr[] = "not";
 
-    new_strcat(firstStr, secondStr);
     // original_strcat(firstStr, secondStr);
+
+    new_strcat(firstStr, secondStr);
+    /*It makes sense here. Now 's' or 'firstStr' is the concatenated result. And 't' or 'secondStr' remains as is.
+    * I think the explanation for this is that s and t are not the same as their pointer values. In new_strcat, s is incremented till it becomes '\0' i.e. null. 
+    * It remains at that value and only its pointer is modified. I still dont know why I cant see the result of the modification within the function, my suspicion is that it is a memory access thing.  
+    */
+    printf("%s\n", firstStr);
+    printf("%s\n", secondStr);
 }
